dacf63e7bbf9af751d14b80cb64465c658a9db4a
Ethan52 <42317139+Ethan52@users.noreply.github.com>
2022-07-05 17:46:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -151,4 +151,63 @@ var graph = {
         {source: "Cultural Society", target: "Attendance Spreadsheet"},
         {source: "Cultural Society", target: "Flyers"}
     ]
+};
+
+var simulation = d3
+    .forceSimulation(graph.nodes)
+    .force(
+        "link",
+        d3.forceLink(graph.links).id(function(d) {
+            return d.name;
+        })
+    )
+    .force("charge", d3.forceManyBody().strength(-30))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
+
+var link = svg
+    .append("g")
+    .selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .attr("stroke-width", function(d) {
+        return 3;
+    })
+    .style("stroke", "pink");
+
+var node = svg 
+    .append("g")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter()
+    .append("circle")
+    .attr("r", 5)
+    .attr("fill", function(d) {
+        return "orange";
+    })
+    .attr("stroke", "black");
+
+function ticked() {
+    link
+        .attr("x1", function(d) {
+            return d.source.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
+    
+    node
+        .attr("cx", function(d) {
+            return d.x;
+        })
+        .attr("cy", function(d) {
+            return d.y;
+        });
 }
